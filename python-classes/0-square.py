437356849b3473eddbebb6155dcee7c1dcdf6ce3
Muhodari <sagemuho@gmail.com>
2024-03-11 10:07:14 +0200

    added python classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" empty class to define square class """
+
+
+class Square:
+    """ empty class to define square class """
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class to define square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square"""
+        self.__size = size
